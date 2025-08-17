6e2fe0c31aad3bf7c404a850539216c33351489d
BaranTascii <barantasci44@gmail.com>
2025-08-17 17:06:16 +0300

    Update

--- react-project/src/components/Product.jsx
+++ react-project/src/components/Product.jsx
@@ -1,14 +1,12 @@
 // src/components/Product.jsx
 
-export const Product = () => {
-	const price = 999;
-	const imgUrl = "<https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640>";
-
+export const Product = props => {
   return (
-    <>
-      <h2>Tacos</h2>
-			<img src={imgUrl} alt="Tacos With Lime" width="640" />
-      <p>Price: {price} credits</p>
-    </>
+    <div>
+      <h2>{props.name}</h2>
+			<img src={props.imgUrl} alt={props.name} width="480" />
+			<p>Price: {props.price} credits</p>
+    </div>
   );
 };
+
